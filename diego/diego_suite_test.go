cfba9697bce2a7beb5553c1374e9fe8f6c3109d3
Amit Gupta <agupta@pivotal.io>
2015-02-27 16:55:38 -0800

    Bump timeouts to see real reasons for failures
    Signed-off-by: Matt Royal <mroyal@pivotal.io>

--- diego/diego_suite_test.go
+++ diego/diego_suite_test.go
@@ -12,12 +12,12 @@ import (
 )
 
 const (
-	CF_PUSH_TIMEOUT    = 1 * time.Minute
-	CF_LOGS_TIMEOUT    = 10 * time.Second
-	CF_SET_ENV_TIMEOUT = 10 * time.Second
-	CF_START_TIMEOUT   = 4 * time.Minute
-	CURL_RETRY_TIMEOUT = 20 * time.Second
-	CURL_TIMEOUT       = 5 * time.Second
+	CF_PUSH_TIMEOUT    = 4 * time.Minute
+	CF_LOGS_TIMEOUT    = 30 * time.Second
+	CF_CURL_TIMEOUT    = 30 * time.Second
+	CF_START_TIMEOUT   = 10 * time.Minute
+	CURL_RETRY_TIMEOUT = 30 * time.Second
+	CURL_TIMEOUT       = 10 * time.Second
 
 	DIEGO_STAGE_BETA = "DIEGO_STAGE_BETA"
 	DIEGO_RUN_BETA   = "DIEGO_RUN_BETA"
