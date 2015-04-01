3ea5e4b53d8fd96e296cab0477dc9c851f7c60d6
Amit Gupta <agupta@pivotal.io>
2015-04-01 15:20:38 -0700

    Use github buildpack so that concurrent staging requests scale [#90332642]
    Signed-off-by: Leo Chu <leocchu@gmail.com>

--- diego/diego_test.go
+++ diego/diego_test.go
@@ -131,7 +131,7 @@ func pushApp(appName, path, instances, memory, pushFilePath, logFilePath string)
 		"push", appName,
 		"-p", path,
 		"--no-start",
-		"-b", "go_buildpack",
+		"-b", "https://github.com/cloudfoundry/go-buildpack.git#v1.2.0",
 		"-i", instances,
 		"-k", "1G",
 		"-m", memory,
