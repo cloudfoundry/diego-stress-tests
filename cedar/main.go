5e550817e411e3acc19682b2585da78cced60b37
Joachim Valdez <joachimvaldez@gmail.com>
2016-08-26 13:33:26 -0400

    wip

--- cedar/main.go
+++ cedar/main.go
@@ -3,16 +3,19 @@ package main
 import (
 	"flag"
 
+	"encoding/json"
 	"golang.org/x/net/context"
+	"time"
 
 	"code.cloudfoundry.org/cflager"
 	"code.cloudfoundry.org/diego-stress-tests/cedar/cli"
 	"code.cloudfoundry.org/diego-stress-tests/cedar/config"
 	"code.cloudfoundry.org/diego-stress-tests/cedar/seeder"
+	"code.cloudfoundry.org/lager"
 )
 
 var (
-	domain = flag.String("domain", "bosh-lite.com", "app domain")
+	domain = flag.String("domain", "", "app domain")
 
 	numBatches       = flag.Int("n", 1, "number of batches to seed")
 	maxInFlight      = flag.Int("k", 1, "max number of cf operations in flight")
@@ -62,8 +65,44 @@ func main() {
 	cfClient := cli.NewCfClient(ctx, *maxInFlight)
 	defer cfClient.Cleanup(ctx)
 
+	if config.Domain == "" {
+		config.Domain = getDefaultSharedDomain(ctx, cfClient)
+	}
+
 	deployer := seeder.NewDeployer(config, apps, cfClient)
 	deployer.PushApps(logger, ctx, cancel)
 	deployer.StartApps(ctx, cancel)
 	deployer.GenerateReport(ctx, cancel)
+}
+
+func getDefaultSharedDomain(ctx context.Context, cfClient cli.CFClient) string {
+	logger, ok := ctx.Value("logger").(lager.Logger)
+	if !ok {
+		logger, _ = cflager.New("cedar")
+	}
+	logger = logger.Session("get-default-shared-domain")
+
+	// cf curl to get shared domains
+	out, err := cfClient.Cf(ctx, 30*time.Second, "curl", "/v2/shared_domains")
+	if err != nil {
+		logger.Error("cf-curl-failed", err)
+	}
+
+	// parse response
+	var f interface{}
+	err = json.Unmarshal(out, &f)
+	if err != nil {
+		logger.Error("json-unmarshal-failed", err)
+	}
+	m := f.(map[string]interface{})
+	resources := (m["resources"]).([]interface{})
+	if len(resources) > 1 {
+		shared_domain := (resources[0]).(map[string]interface{})
+		entity := shared_domain["entity"].(map[string]interface{})
+		name := entity["name"].(string)
+		return name
+	}
+
+	logger.Info("no-shared-domains", lager.Data{})
+	return ""
 }
